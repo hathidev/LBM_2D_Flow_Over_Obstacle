9469b002b8e2b05bfdabda9bcfcda653aee69c85
hathidev <66783536+hathidev@users.noreply.github.com>
2021-07-20 00:22:35 +0530

    Update 2D_LBM.py

--- 2D_LBM.py
+++ 2D_LBM.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 19 09:05:50 2021
-
-@author: Dev Hathi
-"""
-
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -18,8 +11,8 @@ NI=300 #Number of lattices in y-direction
 NSTEPS=10000 #Total number of time-steps
 RE=100 #Reynolds Number
 WI=250 #Write Interval - Number of time-steps after which plots and files are prepared
-WI_DL=50
-DL_COUNT=0
+WI_DL=50 #Write Interval for Drag and Lift forces
+DL_COUNT=0 #Counter 
 
 #Define obstacle in line 91
 #Obstacle parameters
@@ -46,6 +39,7 @@ COL1 = np.array([0,1,2])  #Left wall of Lattice
 COL2 = np.array([3,4,5])  #Interior of Lattice
 COL3 = np.array([6,7,8])  #Right wall of Lattice
 
+#Initialization of Arrays
 FIN=np.zeros((9,NI,NJ))
 FEQ=np.zeros((9,NI,NJ))
 FOUT=np.zeros((9,NI,NJ))
@@ -75,7 +69,7 @@ def CALC_EQUILIBRIUM():   #Calculation of Equilibrium Function
 def CALC_INIVEL(D,X,Y):  #Initial Velocity with slight perturbation
     return (1-D)*ULB* (1 + 1e-4* np.sin(Y/(NJ-1)*2*np.pi))
 
-def CALC_VORTICITY():
+def CALC_VORTICITY(): #Vorticity Calculation
     for i in range(NI):
         for j in range(NJ):
             if i==0 or j==0 or i==(NI-1) or j==(NJ-1):
@@ -83,7 +77,7 @@ def CALC_VORTICITY():
             else:
                 VORTICITY[i,j] = (U[1,i+1,j] - U[1,i-1,j] - U[0,i,j+1] + U[0,i,j-1])/2
                 
-def CALC_DRAG_LIFT():
+def CALC_DRAG_LIFT(): #Calculation of Drag and Lift Forces
     global DL_COUNT
     DRAG[DL_COUNT]=0
     LIFT[DL_COUNT]=0
@@ -116,16 +110,16 @@ def OBSTACLE_AIRFOIL(X,Y):
     return ((X-CX)>0) & ((X-CX)<(CHORD*np.cos(AOA_RAD))) & ((Y-CY)>Y_L) & ((Y-CY)<Y_U) #Equation of NACA00XX Airfoil
 
 
-def BC_OUTLET():
+def BC_OUTLET(): #Outflow Boundary Condition 
     FIN[COL3,-1,:] = FIN[COL3,-2,:]
 
-def BC_INLET():
+def BC_INLET(): #Velocity at Inlet
     U[:,0,:] = VEL[:,0,:]
     RHO[0,:] = 1./(1.-U[0,0,:]) * (np.sum(FIN[COL2,0,:],axis=0) + 2*np.sum(FIN[COL3,0,:],axis=0))
     CALC_EQUILIBRIUM()
     FIN[COL1,0,:] = FEQ[COL1,0,:] + FIN[[8,7,6],0,:] - FEQ[[8,7,6],0,:]
 
-def BC_WALL():
+def BC_WALL(): #Bounce-back
     for k in range(9):
         FOUT[k, OBSTACLE]=FIN[8-k, OBSTACLE]
         
@@ -137,7 +131,7 @@ def STREAMING():
     for k in range(9):
         FIN[k,:,:] = np.roll(np.roll(FOUT[k,:,:],C[k,0],axis=0),C[k,1],axis=1)
         
-def WRITE_FILES():
+def WRITE_FILES(): #Create post-processing files
     f1=open("time_"+str(t)+".dat","w+")
     f1.write('VARIABLES = "X", "Y", "V", "P", "VORTICITY" \n')
     f1.write("ZONE I="+str(NI)+", J="+str(NJ)+" F=POINT \n")
@@ -147,13 +141,13 @@ def WRITE_FILES():
             f1.write(str(i)+" "+str(j)+" "+str(np.sqrt(U[0,i,j]**2+U[1,i,j]**2))+" "+str(P[i,j])+" "+str(VORTICITY[i,j])+"\n")
     f1.close()
 
-def SAVE_PLOTS():
+def SAVE_PLOTS(): #Create post-processing plots
     plt.clf() 
     plt.axis('off')
     plt.imshow(np.sqrt(U[0]**2+U[1]**2).transpose(),cmap=cm.jet)
     plt.savefig("vel_"+str(t)+".png")
     
-def WRITE_DRAG_LIFT():
+def WRITE_DRAG_LIFT(): #Create forces file
     f2=open("drag_lift.dat","w+")
     f2.write('VARIABLES = "T", "DRAG", "LIFT" \n')
     for n in range(DL_COUNT):
@@ -161,8 +155,9 @@ def WRITE_DRAG_LIFT():
     f2.close()    
     
 VEL = np.fromfunction(CALC_INIVEL, (2,NI,NJ))
-OBSTACLE = np.fromfunction(OBSTACLE_CIRCLE, (NI,NJ))
+OBSTACLE = np.fromfunction(OBSTACLE_CIRCLE, (NI,NJ)) #Changable obstacle definition 
 
+#Initialization of solution
 RHO[:,:] = RHOLB
 U[:,:,:]=VEL[:,:,:]
 CALC_EQUILIBRIUM()
